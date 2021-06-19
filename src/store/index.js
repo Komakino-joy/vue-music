158e69c4da1e82ee4b66d55cbed548baad881127
Bryan Alvarez <alvarez.bryan79@yahoo.com>
2021-06-19 14:21:25 -0700

    added getters and mutations to handle modal toggling with vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,20 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    authModalShow: false,
   },
+  // dispatchers
   mutations: {
+    toggleAuthModal: (state) => {
+      state.authModalShow = !state.authModalShow;
+    },
   },
   actions: {
   },
   modules: {
+  },
+  // selectors
+  getters: {
+    // authModalShow: (state) => state.authModalShow,
   },
 });
