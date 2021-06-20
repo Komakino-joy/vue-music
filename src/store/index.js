986e6443b7d5e8405c2006a3e40b8f20bc5af97e
Bryan Alvarez <alvarez.bryan79@yahoo.com>
2021-06-20 14:47:19 -0700

    integrated firestore collections

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,41 @@
 import { createStore } from 'vuex';
+import { auth, usersCollection } from '@/includes/firebase';
 
 export default createStore({
   state: {
     authModalShow: false,
+    userLoggedIn: false,
   },
   // dispatchers
   mutations: {
     toggleAuthModal: (state) => {
       state.authModalShow = !state.authModalShow;
     },
+    toggleAuth: (state) => {
+      state.userLoggedIn = !state.userLoggedIn;
+    },
   },
   actions: {
+    // ctx references the store itself, destructuring commit from ctx
+    async register({ commit }, payload) {
+      const userCred = await auth.createUserWithEmailAndPassword(
+        payload.email, payload.password,
+      );
+
+      await usersCollection.doc(userCred.user.uid).set({
+        name: payload.name,
+        email: payload.email,
+        age: payload.age,
+        country: payload.country,
+      });
+
+      await userCred.user.updateProfile({
+        displayName: payload.name,
+        // profilePicture: null
+      });
+
+      commit('toggleAuth');
+    },
   },
   modules: {
   },
