1e19eccd97ac09a5e96bf8ea5fe57045f1810fea
Bryan Alvarez <alvarez.bryan79@yahoo.com>
2021-06-20 18:03:30 -0700

    implemented routing with vue-router

--- src/main.js
+++ src/main.js
@@ -4,15 +4,21 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import VeeValidatePlugin from './includes/validation';
-import './includes/firebase';
+import { auth } from './includes/firebase';
 import './assets/tailwind.css';
 import './assets/main.css';
 
-const app = createApp(App);
+let app;
 
-app.use(VeeValidate);
-app.use(store);
-app.use(router);
-app.use(VeeValidatePlugin);
+// Wait for firebase to initialize before the app
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App);
+    app.use(VeeValidate);
+    app.use(store);
+    app.use(router);
+    app.use(VeeValidatePlugin);
 
-app.mount('#app');
+    app.mount('#app');
+  }
+});
