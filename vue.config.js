b9ae6c09d0549920838f718423e33eec07735382
Bryan Alvarez <Bryan.Alvarez@genco.com>
2021-06-22 14:04:31 -0700

    Converted into PWA

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,12 @@ module.exports = {
       compositionOnly: true,
       fullInstall: true,
     },
+    pwa: {
+      name: 'Vue-Music',
+      themeColor: '#ff5e3a',
+      manifestOptions: {
+        short_name: 'Music',
+      },
+    },
   },
 };
